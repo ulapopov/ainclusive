91a99c06a740a4667170975b36542f5fb941f8a0
uliana-yummly <uliana.popov@yummly.com>
2024-04-23 15:23:40 -0700

    add alignment based on the language

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from logging import StreamHandler
 from google.cloud import storage
 from google.oauth2 import service_account
 from itertools import zip_longest
+from langdetect import detect
 
 app = Flask(__name__)
 
@@ -215,7 +216,6 @@ def generate_gcs_url(bucket_name, file_path):
     blob = bucket.blob(file_path)
     return blob.public_url
 
-
 @app.route('/display/<category>')
 def serve_content(category):
     base_path = f'{category}/'
@@ -235,15 +235,20 @@ def serve_content(category):
     major_ideas = fetch_text_content_from_gcs(bucket_name, f'{base_path}major_ideas.txt').split('\n')
     new_words = fetch_text_content_from_gcs(bucket_name, f'{base_path}new_words.txt').split('\n')
 
+    # Detect language of text_content
+    language = detect(text_content)
+    align_class = 'align-right' if language == 'he' else 'align-left'
+
     # Create pairs with images, defaulting to 'No Image Available'
     words_and_images = [(word, word_image_dict.get(i + 1, 'No Image Available')) for i, word in enumerate(new_words)]
     ideas_and_images = [(idea, idea_image_dict.get(i + 1, 'No Image Available')) for i, idea in enumerate(major_ideas)]
 
     return render_template('display.html', words_and_images=words_and_images, text=text_content,
-                           ideas_and_images=ideas_and_images,
+                           ideas_and_images=ideas_and_images, align_class=align_class,
                            summaries=fetch_text_content_from_gcs(bucket_name, f'{base_path}text_summary.txt'),
                            game1_txt=fetch_text_content_from_gcs(bucket_name, f'{base_path}fillin.txt'),
                            game2_txt=fetch_text_content_from_gcs(bucket_name, f'{base_path}not_matching.txt'))
+
 
 
 @app.route('/', methods=['GET', 'POST'])
